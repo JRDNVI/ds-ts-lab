5a530aa898b5abd9d1649585ded209d96e0d7978
JRDNVI <20096529.wit@gmail.com>
2024-09-12 22:04:38 +0100

    Function to update friend's interests.

--- src/02-functions.ts
+++ src/02-functions.ts
@@ -81,3 +81,15 @@ function findFriends(
 
 console.log(findFriends(friends, (friend) => friend.name.startsWith('Pa')));
 console.log(findFriends(friends, (friend) => friend.age < 35));
+
+function addInterest(friend: Friend, interest: string) {
+  if (!friend.interests){
+    friend.interests = []
+  }
+
+  friend.interests.push(interest)
+  
+  return friend.interests
+}
+
+console.log(addInterest(friends[1], 'Politics'))
