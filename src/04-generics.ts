ed9743d96c8bfa6cb02c4c1ef52027ce82ce1447
JRDNVI <20096529.wit@gmail.com>
2024-09-13 11:11:20 +0100

    Functions with generics.

--- src/04-generics.ts
+++ src/04-generics.ts
@@ -7,3 +7,18 @@ function findMatch<T>( data : T[], criteria: (d: T) => boolean ) : T | undefined
 
 console.log(findMatch<Friend>(friends, (f) => f.name.startsWith('Jane')  ))
 console.log(findMatch<Colleague>(colleagues.current, (c) => c.department === 'Finance'  ))
+
+
+
+function sort<T>( data : T[], criteria: (a: T, b: T) => number ) : T[] {
+  return [...data].sort(criteria)
+}
+// Sort friends by age
+console.log(sort<Friend>(friends, (a, b) => a.age - b.age));
+// Sort colleagues by extension number
+console.log(
+  sort<Colleague>(
+    colleagues.current,
+    (a, b) => a.contact.extension - b.contact.extension
+  )
+);
